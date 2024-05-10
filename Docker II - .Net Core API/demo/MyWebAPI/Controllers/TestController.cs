b6098afdbb57709827d4db136d222e0fda8f7bef
Scott Gerold <scott@futuresystemsinc.com>
2024-05-10 08:47:02 -0400

    commit

---
+++ Docker II - .Net Core API/demo/MyWebAPI/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebAPI.Controllers
+{
+    [ApiController]
+    [Route("test")]
+
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello Scott Gerold from web API";
+        }
+    }
+}
